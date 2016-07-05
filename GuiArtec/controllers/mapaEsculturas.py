5b04b3dd8fb6d237dd18a17a0fa71eeaf0f22134
fiorellavicentin16 <fiorellavicentin@gmail.com>
2016-07-05 12:03:33 -0300

    Actualizo controlador mapaEscultura,vista senderoEscultura y vista quienes somos

--- GuiArtec/controllers/mapaEsculturas.py
+++ GuiArtec/controllers/mapaEsculturas.py
@@ -19,8 +19,48 @@ def esculturasMarkers():
         places.append(place)
     return response.json(places)
 
+def esculturasMarkersP():
+    places = []
+
+    lat = float(request.vars['lat'])
+    lng = float(request.vars['lng'])
+    distance = float(request.vars['distance'])
+
+    rows = db.executesql('SELECT lat, lng, nombre, (111.302616974 * (lat - ?) * 111.302616974 * (lat - ?) + 111.302616974 * (? - lng) * 111.302616974 * (? - lng)) AS distance FROM esculturag WHERE id IS NOT NULL AND distance < (? * ?) ORDER BY distance;',
+        placeholders = [lat, lat, lng, lng, distance, distance]
+    )
+
+
+    for row in rows:
+        place = {
+        'lat': row[0],
+        'lng': row[1],
+        'title': row[2],
+        'location' : {'lat':row[0],'lng': row[1]},
+        'stopover' : True
+        #'infowindow':{'content':"<p>HTML Content</p>"}
+        }
+        places.append(place)
+    return response.json(places) 
+
+def esculturasMarkers1():
+    places = []
+    rows = db(db.camino.id != None).select()
+    for row in rows:
+        place = {
+        'lat': row.lat,
+        'lng': row.lng,
+        'title': "Nombre y foto",
+        'type': "Escultura",
+        'location' : {'lat':row.lat,'lng': row.lng},
+        'stopover' : True
+        #'infowindow':{'content':"<p>Foto. Nombre</p>"}
+        }
+        places.append(place)
+    return response.json(places)
+
 #Hace falta crear mas senderos como este.
-def crearSenderoEsculturas():
+def crearSenderoEsculturas1():
     paths = []
     rows = db().select(db.camino.ALL,orderby=db.camino.id)
     for row in rows:
@@ -31,22 +71,6 @@ def crearSenderoEsculturas():
         }
     paths.append(path)
     return response.json(paths)
-
-
-def esculturasMarkers2():
-    places = []
-    rows = db(db.scul2.id != None).select()
-    for row in rows:
-        place = {
-        'lat': row.lat,
-        'lng': row.lng,
-        'title': row.nombre,
-        'location' : {'lat':row.lat,'lng': row.lng},
-        'stopover' : True
-        #'infowindow':{'content':"<p>HTML Content</p>"}
-        }
-        places.append(place)
-    return response.json(places)
 
 
 def crearSenderoEsculturas2():
